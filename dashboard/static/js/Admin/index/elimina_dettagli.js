1783bc3559e10208a2b8f975735ae069002e0a21
rfusar <rfusaro12@gmail.com>
2023-12-25 01:47:03 +0100

    0.0.0

--- dashboard/static/js/Admin/index/elimina_dettagli.js
+++ dashboard/static/js/Admin/index/elimina_dettagli.js
@@ -1,28 +1,56 @@
 gestione___modificaANDdettagli(".modifica", "modifica")
 gestione___modificaANDdettagli(".piu_dettagli", "utente")
+gestione___modificaANDdettagli(".elimina", "utente")
 
-//ELIMINA
-document.querySelectorAll('.elimina').forEach((e, i) => {
-    e.addEventListener('click', () => {
-        const R = window.confirm("sei sicuro di eliminare l'utente?")
-        if (R) {
-            fetch("/modificaDB", {
-                method: 'POST',
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({
-                    "email": document.querySelectorAll('.email_utente')[i].textContent,
-                    "elimina": true
+gestione___modificaANDdettagli("#azienda", "#Ragionesociale")
+gestione___modificaANDdettagli("#modificaUtente", "#ruolo")
+
+
+
+
+
+
+function gestione___modificaANDdettagli(classe, pathFisso) {
+    window.addEventListener('click', () => {
+        //*CLASSE
+        if (classe[0] == ".") {
+            if (classe[1] == "e") {
+                document.querySelectorAll(classe).forEach((e, i) => {
+                    e.addEventListener('click', () => {
+                        const R = window.confirm("sei sicuro di eliminare l'utente?")
+                        if (R) {
+                            fetch("/modificaDB", {
+                                method: 'POST',
+                                headers: { "Content-Type": "application/json" },
+                                body: JSON.stringify({
+                                    "email": document.querySelectorAll('.email_utente')[i].textContent,
+                                    "elimina": true
+                                })
+                            }).then(res => res.json()).then(d => { document.querySelector('#risposta').textContent = `${d}... aggiorna la pagina` })
+                        }
+                    })
                 })
-           }).then(res => res.json()).then(d => { document.querySelector('#risposta').textContent = `${d}... aggiorna la pagina` })
+            }
+            else {
+                document.querySelectorAll(classe).forEach((e, i) => {
+                    e.addEventListener('click', () => { e.setAttribute("href", `/${pathFisso}/${document.querySelectorAll('.email_utente')[i].getAttribute("value")}`) })
+
+                })
+            }
         }
+        //*ID
+        else if (classe[0] == "#") {
+            document.querySelector(classe).addEventListener('click', () => {
+                if (classe[1] == "a") {
+                    document.querySelector(classe).setAttribute('href', `/${document.querySelector(pathFisso).getAttribute('value')}/dettaglio`)
+                }
+                else if (classe[1] == "m") {
+                    document.querySelector(classe).setAttribute('href', `/modifica/${document.querySelector(pathFisso).getAttribute('value')}`)
+
+                }
+            })
+
+        }
+
     })
-})
-
-
-function gestione___modificaANDdettagli(classe, pathFisso){
-    window.addEventListener('click', ()=>{
-        document.querySelectorAll(classe).forEach((e, i)=>{
-            e.addEventListener('click', () => { e.setAttribute("href", `/${pathFisso}/${document.querySelectorAll('.email_utente')[i].getAttribute("value")}`) })
-        })
-    }) 
 }
--- dashboard/static/js/Admin/index/script_utenza_img.js
+++ dashboard/static/js/Admin/index/script_utenza_img.js
@@ -1,15 +1,19 @@
-function setImg(path, e){
+function setImg(path, e, contenutoDescizione) {
     const img = document.createElement('img')
-        img.style.width = "25px"
-        img.setAttribute('src', path)
-        img.setAttribute('alt', 'ruolo')
-        e.textContent = ""
-        e.append(img)
+    const descrizione = document.createElement('abbr')
+    descrizione.setAttribute('title', contenutoDescizione)
+    
+    img.style.width = "25px"
+    img.setAttribute('src', path)
+    img.setAttribute('alt', 'ruolo')
+    e.textContent = ""
+    descrizione.append(img)
+    e.append(descrizione)
 }
 
-document.querySelectorAll('.ruolo').forEach(e=>{
-    if(e.textContent == "admin"){ setImg("../../static/img/admin.png", e)}
-    else if(e.textContent == "utente"){ setImg("../../static/img/user.png", e)}
-    else if(e.textContent == "superadmin"){ setImg("../../static/img/superadmin.png", e)}
-    else if(e.textContent == "referente"){ setImg("../../static/img/referente.png", e)}
+document.querySelectorAll('.ruolo').forEach(e => {
+    if (e.textContent == "spike-user") { setImg("../../static/img/admin.png", e, "user-spike") }
+    else if (e.textContent == "user") { setImg("../../static/img/user.png", e, "user") }
+    else if (e.textContent == "spike-admin") { setImg("../../static/img/superadmin.png", e, "spike-admin") }
+    else if (e.textContent == "referent") { setImg("../../static/img/referente.png", e, "referente") }
 })
